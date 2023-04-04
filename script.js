92426d3ab7b0616c50fd373eb6dc75426bfeea6c
Skylar-Lorena <lorayne911@gmail.com>
2023-04-04 23:52:51 +0300

    define form elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// define form elements
+const goalNameInput = document.getElementById('goalName');
+const descriptionInput = document.getElementById('description');
+const deadlineInput = document.getElementById('deadline');
+const addButton = document.querySelector('button[type="submit"]');
