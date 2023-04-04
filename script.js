76cdf3efca9f2de7f87dfb9973f6328134259c8a
Skylar-Lorena <lorayne911@gmail.com>
2023-04-04 23:53:17 +0300

    add event listener to form submit button

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const goalNameInput = document.getElementById('goalName');
 const descriptionInput = document.getElementById('description');
 const deadlineInput = document.getElementById('deadline');
 const addButton = document.querySelector('button[type="submit"]');
+
+// add event listener to form submit button
+addButton.addEventListener('click', (event) => {
