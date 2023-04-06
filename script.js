0c5730a5147a58554a000e85158cfdc5b4a04b20
Skylar-Lorena <lorayne911@gmail.com>
2023-04-06 18:15:15 +0300

    convert my db.json to js code

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const goals = {
+  "goals": [
+    {
+      "goalName": "Keep fit",
+      "description": "Eat well and drink water. Work out",
+      "deadline": "2023-04-28",
+      "id": 1
+    },
+    {
+      "goalName": "Keep sane",
+      "description": "Go out. Take walks. Travel and Be Happy",
+      "deadline": "2023-04-28",
+      "id": 2
+    }
+  ]
+};
+
 // Get the form element and add a submit event listener
 const form = document.querySelector('form');
 form.addEventListener('submit', addGoal);
@@ -8,7 +25,7 @@ const goalsList = document.getElementById('goalsList');
 // Function to handle form submit and add goal to the database
 function addGoal(event) {
   event.preventDefault();
-  
+
   // Get the input values
   const goalNameInput = document.getElementById('goalName');
   const descriptionInput = document.getElementById('description');
@@ -21,51 +38,39 @@ function addGoal(event) {
   const goal = { goalName, description, deadline };
 
   // Make a POST request to the server to add the goal to the database
-  fetch('http://localhost:3000/goals', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(goal)
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Add the new goal to the list
-    const li = document.createElement('li');
-    li.classList.add('card');
-    li.innerHTML = `
-      <h3>${data.goalName}</h3>
-      <p>${data.description}</p>
-      <p>${data.deadline}</p>
-    `;
-    goalsList.appendChild(li);
+  // Since we're not using a server-side database, we'll just add the new goal to our `goals` object
+  goal.id = goals.goals.length + 1;
+  goals.goals.push(goal);
 
-    // Clear the input fields
-    goalNameInput.value = '';
-    descriptionInput.value = '';
-    deadlineInput.value = '';
-  })
-  .catch(error => console.error(error));
+  // Add the new goal to the list
+  const li = document.createElement('li');
+  li.classList.add('card');
+  li.innerHTML = `
+    <h3>${goal.goalName}</h3>
+    <p>${goal.description}</p>
+    <p>${goal.deadline}</p>
+  `;
+  goalsList.appendChild(li);
+
+  // Clear the input fields
+  goalNameInput.value = '';
+  descriptionInput.value = '';
+  deadlineInput.value = '';
 }
 
 // Function to get all goals from the database and display them on the page
 function getGoals() {
-  fetch('http://localhost:3000/goals')
-  .then(response => response.json())
-  .then(goals => {
-    // Loop through the goals and add them to the list
-    goals.forEach(goal => {
-      const li = document.createElement('li');
-      li.classList.add('card');
-      li.innerHTML = `
-        <h3>${goal.goalName}</h3>
-        <p>${goal.description}</p>
-        <p>${goal.deadline}</p>
-      `;
-      goalsList.appendChild(li);
-    });
-  })
-  .catch(error => console.error(error));
+  // Since we're not using a server-side database, we'll just loop through our `goals` object
+  goals.goals.forEach(goal => {
+    const li = document.createElement('li');
+    li.classList.add('card');
+    li.innerHTML = `
+      <h3>${goal.goalName}</h3>
+      <p>${goal.description}</p>
+      <p>${goal.deadline}</p>
+    `;
+    goalsList.appendChild(li);
+  });
 }
 
 // Call the getGoals function when the page loads to display existing goals
